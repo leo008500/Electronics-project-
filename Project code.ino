6c603f52e52e65ad67907407e5920b98686fffc5
Leo joshwa <leojoshwa4@gmail.com>
2026-09-23 10:35:24 +0530

    New code
    Added functionality to read light values and control an LED based on the readings..

--- Project code.ino
+++ Project code.ino
@@ -1,1 +1,23 @@
 
+const int ldrPin = 34;
+const int ledPin = 2;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  int lightValue = analogRead(ldrPin);
+
+  Serial.print("Light Value: ");
+  Serial.println(lightValue);
+
+  if (lightValue < 1500) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+
+  delay(1000);
+}
